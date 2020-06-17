df80ba508f032d2e6e363b1cacbfb15baa32b314
gaturco <turco.gabriel@live.com>
2020-06-17 17:46:04 -0300

    first component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => (
+    <div className="profile-wrapper">
+        <h1>Fernanda Drumond</h1>
+        <h2>Jornalista</h2>
+        <p>teste teste teste teste teste teste teste teste</p>
+    </div>
+)
+
+export default Profile;
