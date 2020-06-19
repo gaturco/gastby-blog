bb06ffb96b35fee8d454d0c7f0e762f3f642c9ab
gaturco <turco.gabriel@live.com>
2020-06-19 17:12:35 -0300

    adicionando estilos e componentes

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,25 @@
+const handle = 'fedrumond'
+const links = [
+    {
+        label: 'Github',
+        url: `https://gituhb.com/${handle}`
+    },
+    {
+        label: 'Twitter',
+        url: `https://twitter.com/${handle}`
+    },
+    {
+        label: 'Youtube',
+        url: `https://www.youtube.com/${handle}`
+    },
+    {
+        label: 'Instagram',
+        url: `https://www.instagram.com/${handle}`
+    },
+    {
+        label: 'Unsplash',
+        url: `https://unsplash.com/${handle}`
+    },
+]
+
+export default links;
